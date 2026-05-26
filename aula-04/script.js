ee15f500181f3301dc303d1b0e01dd3a4cce918b
Bruno Bortolon da Silva <bbortolondasilva@gmail.com>
2026-05-25 22:18:33 -0300

    Update script.js

--- aula-04/script.js
+++ aula-04/script.js
@@ -1,3 +1,3 @@
 // window.alert("Olá Mundo")
-window.confirm("Está gostando de JS?")
-window.prompt("Qual é o seu nome?")
+// window.confirm("Está gostando de JS?")
+// window.prompt("Qual é o seu nome?")
