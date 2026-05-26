884de2a8b8a21a79c5175e250218fb121a93b904
Bruno Bortolon da Silva <bbortolondasilva@gmail.com>
2026-05-25 23:07:48 -0300

    Update script.js

--- aula-06/ex004/script.js
+++ aula-06/ex004/script.js
@@ -4,4 +4,8 @@ var nome = window.prompt("Qual é o seu nome?")
 document.write(`<h2>Seu nome tem ${nome.length} letras!</h2>`)
 // nome.length: Mesma coisa do len do python, captura a quantidade de caracteres ou índices.
 
+// toUpperCase(): Mesmo que o .upper(), transforma o texto em maíuculo.
 document.write(`<h2>Seu nome em maiúsculo ${nome.toUpperCase()}</h2>`)
+
+// toLowerCase(): Mesmo que o .upper(), transforma o texto em minúsculo.
+document.write(`<h2>Seu nome em minúsculo ${nome.toLowerCase()}</h2>`)
