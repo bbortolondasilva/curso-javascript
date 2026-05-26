a06405fd20b0790936a5142c9b0f76928ad00a42
Bruno Bortolon da Silva <bbortolondasilva@gmail.com>
2026-05-25 22:40:30 -0300

    Update script.js

--- aula-06/script.js
+++ aula-06/script.js
@@ -1,1 +1,3 @@
+// Var é usado para declarar uma variável. Neste caso ela recebe o valor inserido no prompt.
 var nome  = window.prompt("Qual é o seu nome?")
+window.alert("É um grande prezer em te conhecer, " + nome) // Concatenação de strings no JS
