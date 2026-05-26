da0297f67d44ee5b7ef44aeb813f71d6a8d00fbc
Bruno Bortolon da Silva <bbortolondasilva@gmail.com>
2026-05-25 22:47:40 -0300

    Update script.js

---
+++ aula-06/ex003/script.js
@@ -0,0 +1,9 @@
+// Aqui está sendo declarada uma variável (var) do tipo Number (serve tanto para o int, quanto para o float)
+var n1 = Number(window.prompt("Digite um número: ")) 
+
+// // Aqui está sendo declarada uma variável (var) do tipo Number (serve tanto para o int, quanto para o float)
+var n2 = Number(window.prompt("Digite outro número: "))
+
+var soma = n1 + n2
+
+window.alert("A soma dos valores é: " + soma)
