be6b33cdfde399553a5561eeb14d57d317a77605
Bruno Bortolon da Silva <bbortolondasilva@gmail.com>
2026-05-25 23:13:17 -0300

    Update script.js

--- aula-06/ex004/script.js
+++ aula-06/ex004/script.js
@@ -9,3 +9,9 @@ document.write(`<h2>Seu nome em maiúsculo ${nome.toUpperCase()}</h2>`)
 
 // toLowerCase(): Mesmo que o .upper(), transforma o texto em minúsculo.
 document.write(`<h2>Seu nome em minúsculo ${nome.toLowerCase()}</h2>`)
+
+// Para alterar a quantidade de casas decimais mostradas usamos o .toFixed(Nº de casas);
+
+// Para mudar caracteres dentro de uma string usamos .replace(caracter que queremos mudar, caracter que queremos pôr no lugar);
+
+// Para fazer formatações como a de dinheiro no formato pt-BR usamos .toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'}).
