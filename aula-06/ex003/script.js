fb178751852a4cfb02365524da24edf0b9b9c675
Bruno Bortolon da Silva <bbortolondasilva@gmail.com>
2026-05-25 22:51:12 -0300

    Update script.js

--- aula-06/ex003/script.js
+++ aula-06/ex003/script.js
@@ -1,5 +1,7 @@
-// Aqui está sendo declarada uma variável (var) do tipo Number (serve tanto para o int, quanto para o float)
+// Aqui está sendo declarada uma variável (var) do tipo Number() -> Converte para número. (serve tanto para o int, quanto para o float). 
 var n1 = Number(window.prompt("Digite um número: ")) 
+
+// Caso queira um tipo em específico, usamos o Number.parseInt() ou parseFloat() para que ele transforme em int ou float.
 
 // // Aqui está sendo declarada uma variável (var) do tipo Number (serve tanto para o int, quanto para o float)
 var n2 = Number(window.prompt("Digite outro número: "))
