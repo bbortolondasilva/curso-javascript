228720d406186ce446acb7afe6b8dee380fae1d7
Bruno Bortolon da Silva <bbortolondasilva@gmail.com>
2026-05-25 23:06:21 -0300

    Update script.js

--- aula-06/ex004/script.js
+++ aula-06/ex004/script.js
@@ -1,2 +1,7 @@
 var nome = window.prompt("Qual é o seu nome?")
 
+// Document.write: Sobrescreve a página com o conteúdo inserido nele.
+document.write(`<h2>Seu nome tem ${nome.length} letras!</h2>`)
+// nome.length: Mesma coisa do len do python, captura a quantidade de caracteres ou índices.
+
+document.write(`<h2>Seu nome em maiúsculo ${nome.toUpperCase()}</h2>`)
