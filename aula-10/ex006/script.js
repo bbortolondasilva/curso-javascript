a1b21f07a7aa67bed3c5c000bb870f354a15bf91
Bruno Bortolon da Silva <bbortolondasilva@gmail.com>
2026-05-29 13:56:46 -0300

    Update
    Declaração das funções do exercício.
    Utilização de "listeners" para fazer a chamada das funções.

---
+++ aula-10/ex006/script.js
@@ -0,0 +1,19 @@
+var a = window.document.getElementById("area")
+
+function clicar() {
+    a.innerText = "Clicou!"
+    a.style.backgroundColor = "red"
+}
+
+function entrar() {
+    a.innerText = "Entrou!"
+}
+
+function sair() {
+    a.innerText = "Saiu!"
+    a.style.backgroundColor = "green"
+}
+
+a.addEventListener("click", clicar)
+a.addEventListener("mouseenter", entrar)
+a.addEventListener("mouseleave", sair)
