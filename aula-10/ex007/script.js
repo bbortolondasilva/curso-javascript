19b716a4a9ab425dc603b28adeeae780bc11b449
Bruno Bortolon da Silva <bbortolondasilva@gmail.com>
2026-05-29 14:27:39 -0300

    Update script.js

--- aula-10/ex007/script.js
+++ aula-10/ex007/script.js
@@ -1,7 +1,7 @@
-var n1 = Number(document.getElementById("inumero1").value)
-var n2 = Number(document.getElementById("inumero2").value)
-
 function somar() {
+    var n1 = Number(document.getElementById("inumero1").value)
+    var n2 = Number(document.getElementById("inumero2").value)
+    
     var valor_soma = n1 + n2
 
     var resultado = document.getElementById("resultado")
