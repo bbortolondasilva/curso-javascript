5e7475cb0cafc6ccd715d242c35138e021617368
Bruno Bortolon da Silva <bbortolondasilva@gmail.com>
2026-05-30 21:29:32 -0300

    Update script.js

--- aula-11/ex010/script.js
+++ aula-11/ex010/script.js
@@ -2,7 +2,11 @@ function calcular() {
     var txtv = Number(document.querySelector("input#ivelocidade").value)
     var res = document.querySelector("div#res")
 
-    res.innerHTML = `<p>Sua velocidade atual é de <strong>${txtv}</strong></p>`
+    res.innerHTML = `<p>Sua velocidade atual é de <strong>${txtv} Km/h</strong></p>`
+
+    if (txtv > 60) {
+        res.innerHTML += `<p>Você está <strong>multado</strong> por excesso de velocidade!</p>`
+    }
 
     res.innerHTML += `<p>Dirija sempre com cinto de segurança!</p>`
 }
