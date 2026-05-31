73abf3bd32cb8f82c4fdd86f4e128b1ec9617f49
Bruno Bortolon da Silva <bbortolondasilva@gmail.com>
2026-05-31 12:11:01 -0300

    Update script.js

--- aula-12/ex001/script.js
+++ aula-12/ex001/script.js
@@ -2,10 +2,8 @@ var idade = 22
 
 if (idade < 18) {
     console.log("Não vota!")
-} else {
-    if (idade < 18) {
+} else if (idade < 18 || idade > 65) {
         console.log("Voto opcional!")
-    } else {
-        console.log("Voto obrigatório!")
-    }
+} else {
+    console.log("Voto obrigatorio!")
 }
