dedf8ffbb0ad2b12d7c57b29755d54e0add15065
Bruno Bortolon da Silva <bbortolondasilva@gmail.com>
2026-06-03 22:46:22 -0300

    Update

--- desafios/ex002/script.js
+++ desafios/ex002/script.js
@@ -1,4 +1,43 @@
 function verificar() {
     var ano =  new Date().getFullYear()
-    var fAno = document.getElementById("textano")
+    var fAno = document.getElementById("iano")
+    var res = document.querySelector("div#res")
+
+    if (fAno.value.length == 0 || fAno.value > ano) {
+        window.alert("[ERRO] Verifique os dados e tente novamente!")
+    } else {
+        var fSex = document.getElementsByName("radsex")
+        var idade = ano - Number(fAno.value)
+        var genero = ""
+        var img = document.createElement("div")
+        img.setAttribute("id", "foto")
+
+        if (fSex[0].checked) {
+            genero = "Homem"
+
+            if (idade >= 0 && idade < 10) {
+                //criança
+            } else if (idade < 21) {
+                // joven
+            } else if (idade < 50) {
+                // adulto
+            } else {
+                // idoso
+            }
+        } else {
+            genero = "Mulher"
+
+            if (idade >= 0 && idade < 10) {
+                //criança
+            } else if (idade < 21) {
+                // joven
+            } else if (idade < 50) {
+                // adulto
+            } else {
+                // idoso
+            }
+        }
+
+        res.innerHTML = `Detectamos ${genero} com ${idade} anos.`
+    }
 }
