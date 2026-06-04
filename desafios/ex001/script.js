5f09d6c09042f551cd125c043eb20c7adbd679b6
Bruno Bortolon da Silva <bbortolondasilva@gmail.com>
2026-06-03 21:54:07 -0300

    BugFix

--- desafios/ex001/script.js
+++ desafios/ex001/script.js
@@ -1,14 +1,15 @@
-var hora_atual = Number(new Date().getHours())
+var hora_atual = document.getElementById("hora-atual")
 var imagem_hora = document.getElementById("img-hora")
+var hora = new Date().getHours()
+
+hora_atual.innerHTML = `<h2>Agora são ${hora} horas.</h2>`
 
 function define_imagem() {
-    if (hora_atual >= 0 && hora_atual <= 12) {
-        imagem_hora.style.backgroundImage = "assets/imagem-bom-dia.jfif"
-    } else if (hora_atual < 18) {
-        imagem_hora.style.backgroundImage = "assets/imagem-boa-tarde.jpg"
+    if (hora >= 0 && hora <= 12) {
+        imagem_hora.style.backgroundImage = `url("assets/imagem-bom-dia.jfif")`;
+    } else if (hora <= 18) {
+        imagem_hora.style.backgroundImage = `url("assets/imagem-boa-tarde.jpg")`;
     } else {
-        imagem_hora.style.backgroundImage = "assets/imagem-boa-noite.jpg"
+        imagem_hora.style.backgroundImage = `url("assets/imagem-boa-noite.jpg")`;
     }
 }
-
-define_imagem()
