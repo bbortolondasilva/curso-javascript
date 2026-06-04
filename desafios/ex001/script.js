4f282ffc3a8eb5b7f37dc4c92e83a6c95682d4f9
Bruno Bortolon da Silva <bbortolondasilva@gmail.com>
2026-06-03 21:57:06 -0300

    Update

--- desafios/ex001/script.js
+++ desafios/ex001/script.js
@@ -7,9 +7,15 @@ hora_atual.innerHTML = `<h2>Agora são ${hora} horas.</h2>`
 function define_imagem() {
     if (hora >= 0 && hora <= 12) {
         imagem_hora.style.backgroundImage = `url("assets/imagem-bom-dia.jfif")`;
+
+        document.body.style.background = "#fff78b"
     } else if (hora <= 18) {
         imagem_hora.style.backgroundImage = `url("assets/imagem-boa-tarde.jpg")`;
+
+        document.body.style.background = "#d16d33"
     } else {
         imagem_hora.style.backgroundImage = `url("assets/imagem-boa-noite.jpg")`;
+
+        document.body.style.background = "#001046"
     }
 }
