0ff16a55c56c72a6882540a097c924287abb1f98
Bruno Bortolon da Silva <bbortolondasilva@gmail.com>
2026-06-04 10:59:05 -0300

    Update ex014.js

--- aula-13/ex014/ex014.js
+++ aula-13/ex014/ex014.js
@@ -1,6 +1,14 @@
 var c = 1 
 
-while (c <= 6) {
+// Testa se o valor passou do limite e depois executa o código:
+// while (c <= 6) {
+//     console.log("Tudo bem?")
+//     c++
+// }
+
+// Executa o código e depois testa se o valor passou do limite:
+do {
     console.log("Tudo bem?")
     c++
-}
+} while (c <= 10)
+    
