9006636397273ccb1527d35cf1c4fa28006b233d
Bruno Bortolon da Silva <bbortolondasilva@gmail.com>
2026-06-05 16:58:30 -0300

    Update

--- desafios/ex003/script.js
+++ desafios/ex003/script.js
@@ -1,9 +1,7 @@
-var inicio = document.querySelector("input#iini").value
+var inicio = document.getElementById("iini").value
 var fim = document.querySelector("input#ifim").value
 var passo = document.querySelector("input#ipas").value
 
-function mostrarConsole() {
-    console.log(inicio)
-    console.log(fim)
-    console.log(passo)
-}
+console.log(inicio)
+console.log(fim)
+console.log(passo)
