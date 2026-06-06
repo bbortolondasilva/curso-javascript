e02dff4b5084c6ca2c5f8f702378103c72b21056
Bruno Bortolon da Silva <bbortolondasilva@gmail.com>
2026-06-06 15:03:26 -0300

    Create

--- aula-15/ex016/ex016.js
+++ aula-15/ex016/ex016.js
@@ -1,3 +1,25 @@
-let num = []
+let num = [5, 8, 2]
+// .push(): Funciona exatamente como o .append do python.
+num.push(4)
 
-console.log(num)
+
+
+console.log(`Nosso vetor é o ${num}`)
+
+// .length: é o mesmo que o lens do python. Porém não se usa parênteses, pois ele é um atributo, apenas colocamos o nome_variável.length
+console.log(`O vetor tem ${num.length} posições`)
+
+// Selecionando apenas um indice do vetor: "[indice]"
+console.log(`O primeiro elemento do vetor é ${num[0]}`)
+
+// Mostrando o vetor organizado: 
+// .sort(): mesma função que no python, organiza os elementos em ordem crescente
+num.sort()
+
+console.log(`O vetor em ordem crescente fica ${num}\n`)
+
+// mostrando o vetor formatado:
+
+for (var c = 0; c < num.length; c++) {
+    console.log(`- Posição ${c+1}: ${num[c]}`)
+}
---
+++ aula-15/ex016/ex017.js
@@ -0,0 +1,8 @@
+let num = [3, 5, 6, 0, 1, 2, 7]
+
+num.sort()
+
+// mostrando o vetor formatado:
+for (var c = 0; c < num.length; c++) {
+    console.log(`- Posição ${c}: ${num[c]}`)
+}
