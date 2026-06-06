038b946c68407e616fdda21a9636ee5df9247e0f
Bruno Bortolon da Silva <bbortolondasilva@gmail.com>
2026-06-06 15:08:12 -0300

    Update ex017.js

--- aula-15/ex016/ex017.js
+++ aula-15/ex016/ex017.js
@@ -2,7 +2,14 @@ let num = [3, 5, 6, 0, 1, 2, 7]
 
 num.sort()
 
-// mostrando o vetor formatado:
+// mostrando o vetor formatado método 1:
 for (var c = 0; c < num.length; c++) {
     console.log(`- Posição ${c}: ${num[c]}`)
+}
+
+console.log("\n")
+
+// mostrando o vetor formatado método 2:
+for (let key in num) {
+    console.log(`- Posição ${key}: ${num[key]}`)
 }
