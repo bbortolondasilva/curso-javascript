6b163c8af001bcce2ed8079abfadea3e92290bcd
Bruno Bortolon da Silva <bbortolondasilva@gmail.com>
2026-06-06 09:58:12 -0300

    Update
    Validação de preenchimento dos campos do formulário

--- desafios/ex003/script.js
+++ desafios/ex003/script.js
@@ -1,7 +1,12 @@
 function contar() {
-    var inicio = Number(document.getElementById("iini").value)
-    var fim = Number(document.querySelector("input#ifim").value)
-    var passo = Number(document.querySelector("input#ipas").value)
+    var inicio = document.getElementById("iini")
+    var fim = document.querySelector("input#ifim")
+    var passo = document.querySelector("input#ipas")
+    var res = document.getElementById("res")
+
+    if (inicio.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
+        window.alert("ERRO! FALTAM DADOS!")
+    }
 }
 
 document.getElementById("btn").addEventListener("click", contar)
