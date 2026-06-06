a1337a2c45897637d479e6f99ba9c0969932ac49
Bruno Bortolon da Silva <bbortolondasilva@gmail.com>
2026-06-06 10:20:58 -0300

    Update script.js

--- desafios/ex003/script.js
+++ desafios/ex003/script.js
@@ -5,7 +5,29 @@ function contar() {
     var res = document.getElementById("res")
 
     if (inicio.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
-        window.alert("ERRO! FALTAM DADOS!")
+        res.innerHTML = "Impossível contar!"
+        // window.alert("ERRO! FALTAM DADOS!")
+    } 
+
+    else {
+        res.innerHTML = "<p>Contando: </p> <br>"
+
+        var i = Number(inicio.value)
+        var f = Number(fim.value)
+        var p = Number(passo.value)
+
+        if (i < f) {
+            for (i; i <= f; i += p) {
+                res.innerText += `${i} ➡ `
+            }
+        }
+        
+        else {
+            for (i; i >= f; i -= p) {
+                res.innerText += `${i} ➡ `
+            }
+        }
+        res.innerText += `🏁`
     }
 }
 
